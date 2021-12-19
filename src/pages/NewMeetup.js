3f8cead0707341725cef9e9e61df08ddab3d71cb
Waldemar Ewert <waldi.ewert@googlemail.com>
2021-12-19 20:08:04 +0100

    create http request with fetch()

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://backent-for-test-react-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
   return (
     <section>
       <h1>Add new Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
   );
 }
